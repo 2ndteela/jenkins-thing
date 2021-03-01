a0339173f0ea40e2c4f2cbd207092541bede775a
Jeremy <jteela@ls.byu.edu>
2021-03-01 15:57:45 -0700

    final

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -24,8 +24,7 @@ class Calculator {
 
     /*
     Returns the n'th number in the fibonacci sequence
-    https://en.wikipedia.org/wiki/Fibonacci_number
-    Example below
+
     n = x
     0 = 0
     1 = 1
